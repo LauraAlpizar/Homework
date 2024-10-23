a7f291abe92d84e0b4361fc57c8dedb0d152ace2
LauraAlpizar <mlauraalpirodr108@gmail.com>
2024-10-23 10:39:23 -0600

    Delete semana3 directory

--- semana3/main.java/src/Models/Enums/Gender.java
+++
@@ -1,9 +0,0 @@
-package Models.Enums;
-
-// Definimos el enum 'Gender' que representa el género de un personaje.
-public enum Gender {
-    Male,       // El personaje es masculino.
-    Female,     // El personaje es femenino.
-    Genderless, // El personaje no tiene género.
-    unknown     // El género del personaje es desconocido (probablemente por errores en la API o no se especifica).
-}
--- semana3/main.java/src/Models/Enums/Status.java
+++
@@ -1,9 +0,0 @@
-package Models.Enums;
-
-// Definimos el enum 'Status' que representa el estado de un personaje.
-public enum Status {
-    Alive,   // El personaje está vivo.
-    Dead,    // El personaje está muerto.
-    unknown, // Estado desconocido en minúsculas (podría ser un error de la API).
-    Unknown  // Estado desconocido en mayúsculas (para compatibilidad con la API, aunque parece duplicado).
-}
